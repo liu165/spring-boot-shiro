3aeea7133560e97a6407840eb806dbbaa120744f
liukai999 <33122204+liukai999@users.noreply.github.com>
2020-01-10 10:38:15 +0800

    Initial commit

---
+++ src/main/java/com/neo/dao/UserInfoDao.java
@@ -0,0 +1,10 @@
+package com.neo.dao;
+
+import com.neo.model.UserInfo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserInfoDao extends CrudRepository<UserInfo,Long> {
+    /**通过username查找用户信息;*/
+    //进行用户认证
+    public UserInfo findByUsername(String username);
+}
